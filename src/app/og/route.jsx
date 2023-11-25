8e93a61f99bc280b3fca8a9da246544688d8993b
nushankodikara <nushankodi@gmail.com>
2023-11-25 11:50:38 +0530

    added dynamic domain

--- src/app/og/route.jsx
+++ src/app/og/route.jsx
@@ -5,18 +5,25 @@ export const runtime = "edge";
 export async function GET(request) {
     const { searchParams } = new URL(request.url);
     const name = searchParams.get("name");
+
+    const domain = `${request.headers.get(
+        "x-forwarded-proto"
+    )}://${request.headers.get("host")}`;
+    console.log(domain);
+
     const fontData1 = await fetch(
         new URL("/public/fonts/Tangerine-Bold.ttf", import.meta.url)
     ).then((res) => res.arrayBuffer());
     const fontData2 = await fetch(
         new URL("/public/fonts/Quicksand-Bold.ttf", import.meta.url)
     ).then((res) => res.arrayBuffer());
+
     return new ImageResponse(
         (
             <div
                 tw="flex flex-col w-full h-full text-[#ffc034]"
                 style={{
-                    backgroundImage: "url(http://localhost:3000/wish.jpeg)",
+                    backgroundImage: `url(${domain}/wish.jpeg)`,
                 }}
             >
                 <div tw="flex flex-col w-3/5 py-16 px-16 justify-center h-full">
